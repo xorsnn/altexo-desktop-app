e391dd96d5b585754d78074249c0a51f680ac9a8
Sergey Grigorev <none@none>
2016-06-28 08:41:12 +0300

    migrate to cmake, move to nested structure
    --HG--
    branch : dev

--- al_ui/src/main.cpp
+++ al_ui/src/main.cpp
@@ -4,8 +4,11 @@
 #include "imgui/imgui.h"
 #include "imgui_impl_sdl.h"
 #include <stdio.h>
+#include <iostream>
 #include <SDL.h>
 #include <SDL_opengl.h>
+
+#define IM_ARRAYSIZE(_ARR)  ((int)(sizeof(_ARR)/sizeof(*_ARR)))
 
 int main(int, char**)
 {
@@ -41,10 +44,11 @@ int main(int, char**)
   //io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
 
   bool show_test_window = true;
-  bool show_another_window = false;
+
   ImVec4 clear_color = ImColor(114, 144, 154);
 
   // Main loop
+
   bool done = false;
   while (!done)
   {
@@ -55,26 +59,30 @@ int main(int, char**)
       if (event.type == SDL_QUIT)
       done = true;
     }
+
     ImGui_ImplSdl_NewFrame(window);
 
     // 1. Show a simple window
     // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets appears in a window automatically called "Debug"
     {
-      static float f = 0.0f;
-      ImGui::Text("Hello, world!");
-      ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
-      ImGui::ColorEdit3("clear color", (float*)&clear_color);
-      if (ImGui::Button("Test Window")) show_test_window ^= 1;
-      if (ImGui::Button("Another Window")) show_another_window ^= 1;
-      ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-    }
+      // ImGui::SetNextWindowSize(ImVec2(200,100), ImGuiSetCond_FirstUseEver);
+      // ImGui::SetNextWindowPos(ImVec2(100, 100), ImGuiSetCond_FirstUseEver);
+      ImGui::SetNextWindowSize(ImVec2(100,400), ImGuiSetCond_Once);
+      ImGui::SetNextWindowPos(ImVec2(10,10), ImGuiSetCond_Once);
 
-    // 2. Show another simple window, this time using an explicit Begin/End pair
-    if (show_another_window)
-    {
-      ImGui::SetNextWindowSize(ImVec2(200,100), ImGuiSetCond_FirstUseEver);
-      ImGui::Begin("Another Window", &show_another_window);
-      ImGui::Text("Hello");
+      ImGui::Begin("Contacts", NULL);
+      static int selected = 0;
+      ImGui::BeginChild("left pane", ImVec2(-1, 0), true);
+      for (int i = 0; i < 20; i++)
+      {
+        char label[128];
+        sprintf(label, "MyObject %d", i);
+        if (ImGui::Selectable(label, selected == i)) {
+          selected = i;
+          std::cout << selected << std::endl;
+        }
+      }
+      ImGui::EndChild();
       ImGui::End();
     }
 
