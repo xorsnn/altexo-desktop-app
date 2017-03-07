5b05be9d80246743a24f4d34415036a5f3ec94fd
Sergey Grigorev <xors.nn@gmail.com>
2017-03-07 22:34:43 +0300

    initial setup fix

--- helper_scripts/compile/install_cpr.sh
+++ helper_scripts/compile/install_cpr.sh
@@ -3,7 +3,10 @@ cd "${0%/*}"
 
 mkdir -p libs
 cd libs
-git clone https://github.com/whoshuu/cpr.git
+git clone git@github.com:xorsnn/cpr.git
+cd cpr
+git checkout altexo-branch
+cd ..
 mkdir -p build
 cd build
 cmake ../cpr/ -DBUILD_CPR_TESTS=OFF -DUSE_SYSTEM_CURL=ON
